b31204c098acf8212231e7b2f7371bda122597cf
John Green <jkgreen@uclan.ac.uk>
2020-01-09 02:52:23 +0000

    Added settings menu on home screen

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using TMPro;
+
 using static WorldGenerator;
 
 public class MainMenu : MonoBehaviour
@@ -21,6 +25,17 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private float IslandFrequancy = 0.0f;
     [SerializeField] private float IslandWaterHeight = 0.0f;
     [SerializeField] private Vector2Int IslandSize = new Vector2Int();
+
+    // Settings
+    [SerializeField] private GameObject SettingsPanel = null;
+    [SerializeField] private TMP_Dropdown GraphicsQualityDropdown = null;
+    [SerializeField] private Slider MainMusicSlider = null;
+    [SerializeField] private Slider UISlider = null;
+
+    [SerializeField] private AudioMixer MusicMixer = null;
+    [SerializeField] private AudioMixer UIMixer = null;
+
+
 
     private GenerationPayload BackgroundGenerationPayload;
 
@@ -43,6 +58,37 @@ public class MainMenu : MonoBehaviour
 
         BackgroundEnviroment.GenerateWorld(mCharacter, BackgroundGenerationPayload);
         BackgroundEnviroment.transform.RotateAround(BackgroundEnviroment.transform.position, transform.up, IslandStartRotation);
+
+        // setup settings
+        {
+
+            string[] names = QualitySettings.names;
+            GraphicsQualityDropdown.ClearOptions();
+            List<TMP_Dropdown.OptionData> qualityOptions = new List<TMP_Dropdown.OptionData>();
+            int currentQuality = QualitySettings.GetQualityLevel();
+            foreach (string qualityName in names)
+            {
+                TMP_Dropdown.OptionData optionData = new TMP_Dropdown.OptionData(qualityName);
+                qualityOptions.Add(optionData);
+            }
+            GraphicsQualityDropdown.AddOptions(qualityOptions);
+            GraphicsQualityDropdown.value = currentQuality;
+        }
+    }
+
+    public void MusicSettingSliderchanged()
+    {
+        MusicMixer.SetFloat("MusicVol", MainMusicSlider.value);
+    }
+
+    public void UISettingSliderchanged()
+    {
+        UIMixer.SetFloat("UIVol", UISlider.value);
+    }
+
+    public void GraphicsQualityChanged()
+    {
+        QualitySettings.SetQualityLevel(GraphicsQualityDropdown.value);
     }
 
     // Update is called once per frame
@@ -54,8 +100,14 @@ public class MainMenu : MonoBehaviour
 
     public void NewGame()
     {
+        SettingsPanel.SetActive(false);
         SceneManager.LoadScene("WorldCreator", LoadSceneMode.Additive);
         MenuEnabled = false;
+    }
+
+    public void SettingsToggle()
+    {
+        SettingsPanel.SetActive(!SettingsPanel.activeSelf);
     }
 
     public void Exit()
